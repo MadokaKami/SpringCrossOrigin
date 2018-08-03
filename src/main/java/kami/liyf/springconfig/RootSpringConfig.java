44e7657a30790faf3d000a30681dc86822184d68
liyf <804590954@qq.com>
2018-08-03 17:46:09 +0800

    提交

---
+++ src/main/java/kami/liyf/springconfig/RootSpringConfig.java
@@ -0,0 +1,10 @@
+package kami.liyf.springconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("kami.liyf")
+public class RootSpringConfig {
+
+}
